6125c774c52579762def0c4c01f46d26be593384
Felipe <felipe.alvarez@.sociuschile.com>
2025-01-09 17:01:57 -0300

    ta bonita la wa

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function loadComponent(file, elementId) {
-    fetch(file)
-        .then(response => response.text())
-        .then(data => document.getElementById(elementId).innerHTML = data)
-        .catch(err => console.error("Error loading component:", err));
-}
-
